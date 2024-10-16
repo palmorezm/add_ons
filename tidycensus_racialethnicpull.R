fac782ca6bf9afe02edaab6e4d73d2176b4a2c1e
palmorezm <zachary.palmore20@spsmail.cuny.edu>
2024-10-16 12:35:20 -0500

    initial function ideas

---
+++ tidycensus_racialethnicpull.R
@@ -0,0 +1,19 @@
+require(tidycensus)
+
+# function ideas
+
+# funtion 1 - pulls a slew of race/ethnic data
+# names appropriatly based on most common categories from ACS
+# 
+
+# function 2 - pulls regular "total population" data for the state of WI across year 
+# user selects years (as a column/field)
+# user selects states (as a column/field)
+# it automatically fills ac5 whenever possible
+# it automatically fills most common inputs of tidycensus acs function
+# outputs rdata 
+
+# function 3 - exports all data as spreadsheet(s)
+# write rdata from any function to your current working directory
+# or user-specified file path location
+
